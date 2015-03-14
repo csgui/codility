267e4ac5e9f6e71aebc1bbe42d8efd46d257dec2
christian guimaraes <cguimaraes.br@gmail.com>
2015-03-14 16:18:12 +0000

    final solution

--- time-complexity/frog-jump/frog-jump.rb
+++ time-complexity/frog-jump/frog-jump.rb
@@ -1,14 +1,26 @@
-def solution x, y, d
-  raise "X > Y" if x > y
+class XValueError < ArgumentError; end
 
-  y % d == 0 ? y / d : (x + y) / d
-rescue Exception => e
-  puts "Invalid condition:"
-  puts e
+def frog_jump(x, y, d)
+  fail XValueError, "X value should be greater than Y value." if x > y
+  ((y.to_f - x.to_f ) / d.to_f).ceil
 end
 
 require 'minitest/autorun'
 
-class Test
+class Tests < MiniTest::Unit::TestCase
+  def test_example_input
+    assert_equal 3, frog_jump(10, 85, 30)
+  end
 
+  def test_no_jump_needed
+    assert_equal 0, frog_jump(10, 10, 30)
+  end
+
+  def test_exact_jump
+    assert_equal 3, frog_jump(0, 90, 30)
+  end
+
+  def test_x_and_y_values
+    assert_raises(XValueError) { frog_jump(10, 1, 10) }
+  end
 end
